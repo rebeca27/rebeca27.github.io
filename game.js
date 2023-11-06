607ec0e93c85a08c7c67b79d0c0aca9a9aa58427
rebeca27 <38529211+rebeca27@users.noreply.github.com>
2023-11-06 22:16:33 +0200

    Update game.js

--- game.js
+++ game.js
@@ -11,67 +11,72 @@ var config = {
     }
 };
 
+// Initialize the game with your configuration
+var game = new Phaser.Game(config);
+
 function preload() {
-    // Load your assets here
     this.load.image('medievalMap', 'medieval_map.png');
     this.load.spritesheet('knight', 'knight_spritesheet.png', { frameWidth: 32, frameHeight: 48 });
 
     // Properly manage the loading screen visibility
-    var loadingScreen = document.getElementById('loadingScreen');
-    if (loadingScreen) {
-        this.load.on('complete', function() {
-            loadingScreen.style.display = 'none';
-        });
-    }
+    this.load.on('complete', function() {
+        document.getElementById('loadingScreen').style.display = 'none';
+    });
 }
 
 function create() {
-    // Add the medieval map to the game
+    // Add the map as a background
     this.add.image(400, 300, 'medievalMap');
 
     // Add the knight sprite to the game
-    let knight = this.add.sprite(400, 300, 'knight');
+    let knight = this.add.sprite(400, 300, 'knight').setOrigin(0.5, 0.5);
+
+    // Style for the buttons
+    let textStyle = {
+        fontSize: '32px',
+        fill: '#fff', // Text color
+        backgroundColor: '#007bff', // Button background color
+        padding: {
+            x: 10,
+            y: 5,
+        },
+        fontFamily: 'Arial' // You can use any font loaded in your HTML
+    };
 
     // Create interactive buttons
-    let btnMedieval = this.add.text(100, 100, 'Medieval Era', { fill: '#0f0' })
+    let btnMedieval = this.add.text(100, 100, 'Medieval Era', textStyle)
         .setInteractive()
-        .on('pointerdown', () => this.selectEra('medieval'));
+        .on('pointerdown', () => this.selectEra('medieval'))
+        .on('pointerover', () => btnMedieval.setStyle({ fill: '#ff0' })) // Change color on hover
+        .on('pointerout', () => btnMedieval.setStyle({ fill: '#fff' })); // Change color back on out
 
-    let btnTreaty = this.add.text(100, 200, 'Treaty of Versailles', { fill: '#0f0' })
+    let btnTreaty = this.add.text(100, 200, 'Treaty of Versailles', textStyle)
         .setInteractive()
-        .on('pointerdown', () => this.startTreatyEvent());
+        .on('pointerdown', () => this.startTreatyEvent())
+        .on('pointerover', () => btnTreaty.setStyle({ fill: '#ff0' })) // Change color on hover
+        .on('pointerout', () => btnTreaty.setStyle({ fill: '#fff' })); // Change color back on out
 }
 
 function update(time, delta) {
-    // Your game loop goes here
+    // Game loop logic goes here
 }
 
 function selectEra(era) {
-    // Load the appropriate map for the selected era
     console.log('Era selected:', era);
     this.add.image(400, 300, era + 'Map');
 }
 
 function startTreatyEvent() {
     console.log('Treaty of Versailles event started');
-    this.displayDialogue('Choose your approach to the Treaty of Versailles:', ['Harsh Penalties', 'Fair Peace', 'Lenient Approach']);
+    displayDialogue('Choose your approach to the Treaty of Versailles:', ['Harsh Penalties', 'Fair Peace', 'Lenient Approach']);
 }
 
 function displayDialogue(text, options) {
-    // Display the dialogue options
-    let dialogueText = this.add.text(100, 100, text, { fill: '#fff' });
-    options.forEach((option, index) => {
-        this.add.text(100, 150 + (index * 50), option, { fill: '#0f0' })
-            .setInteractive()
-            .on('pointerdown', () => this.optionSelected(option));
-    });
+    // Your dialogue display logic
 }
 
 function optionSelected(choice) {
-    // Handle the player's choice
-    console.log('Player chose:', choice);
-    // Implement the outcomes based on the choice
+    // Your option selected logic
 }
 
-// Initialize the game with your configuration
-var game = new Phaser.Game(config);
+// The game variable is already defined in the config, no need to redefine it.
